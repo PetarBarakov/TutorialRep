7c0b354e3066bde353bdba6775902502fed3403e
Petar Barakov <barakov.petar@gmail.com>
2023-04-02 15:51:04 +0100

    Another gitignore check

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,11 +3,14 @@
 
 int main()
 {   
-    std::cout << "What is your name: ";
-
     std::string name;
-
+    int num;
+    
+    std::cout << "What is your name: ";
     std::cin >> name;
-
     std::cout << "Hello " << name << std::endl;
+
+    std::cout << "Give me a number: ";
+    std::cin >> num;
+    std::cout << "Here is the number doubled: " << num * 2 << std::endl;
 }
